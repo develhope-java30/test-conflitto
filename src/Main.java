335fe4b812c51914bf1a934d27982a82e7bb2c7c
Gino Santonicola <luigisantonicola1988@gmail.com>
2024-12-12 11:43:20 +0100

    add Gino commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(""); //inserire nome e cognome con il primo verso della vostra canzone preferita.
+        System.out.println("Gino Santonicola, Eccoti sai ti stavo proprio aspettando\n" +
+                "ero qui ti aspettavo da tanto tempo\n" +
+                "tanto che stavo per andarmene\n" +
+                "e invece ho fatto bene\n" +
+                "sei il primo mio pensiero che\n" +
+                "al mattino mi sveglia"); //inserire nome e cognome con il primo verso della vostra canzone preferita.
     }
 }
