960b85e4d498f11afbbb0383f9836981610e2bbb
ardit <breguardit1992@gmail.com>
2024-12-12 13:01:23 +0100

    ardit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Gino Santonicola, Eccoti sai ti stavo proprio aspettando\n" +
-                "ero qui ti aspettavo da tanto tempo\n" +
-                "tanto che stavo per andarmene\n" +
-                "e invece ho fatto bene\n" +
-                "sei il primo mio pensiero che\n" +
-                "al mattino mi sveglia"); //inserire nome e cognome con il primo verso della vostra canzone preferita.
+        System.out.println("Ardit");
     }
 }
