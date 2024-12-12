934b417453bc6922cf2c44722c545c20cc3834e9
Merry <rozzimario18@gmail.com>
2024-12-12 11:36:22 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(""); //inserire nome e cognome con il primo verso della vostra canzone preferita.
+    }
+}
