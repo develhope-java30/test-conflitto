f54bdb16b1ae9253e762c53c899a8c03fdd0b5df
Merry <rozzimario18@gmail.com>
2024-12-12 11:56:30 +0100

    fix: changed println per word

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mario Rozzi / Two-faced, caught in the middle"); //inserire nome e cognome con il primo verso della vostra canzone preferita.
+        System.out.println("Mario");//inserire nome e cognome con il primo verso della vostra canzone preferita.
+        System.out.println("Rozzi");
+        System.out.println("Two-faced");
+        System.out.println("caught");
+        System.out.println("in");
+        System.out.println("the");
+        System.out.println("middle");
     }
 }
