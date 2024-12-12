1bb5d2eaadd1e356087caa0e9ac3603728281b86
DjGuruge <djguruge@hotmail.it>
2024-12-12 12:00:34 +0100

    Aggiunto del mio pezzo

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Gino Santonicola, Eccoti sai ti stavo proprio aspettando\n" +
-                "ero qui ti aspettavo da tanto tempo\n" +
-                "tanto che stavo per andarmene\n" +
-                "e invece ho fatto bene\n" +
-                "sei il primo mio pensiero che\n" +
-                "al mattino mi sveglia"); //inserire nome e cognome con il primo verso della vostra canzone preferita.
+        System.out.println("Vincent Guruge, Tutti gli Stati, tutti i dominii che hanno avuto, e hanno imperio sopra gli uomini, sono stati e sono o Repubbliche o Principati. I principati sono o ereditari, de’ quali il sangue del loro Signore ne sia stato lungo tempo Principe, o e’ sono nuovi. I nuovi o sono nuovi tutti, come fu Milano a Francesco Sforza, o sono come membri aggiunti allo stato ereditario del Principe che gli acquista, come è il Regno di Napoli al Re di Spagna. Sono questi dominii, così acquistati, o consueti a vivere sotto un Principe, o usi ad esser liberi; ed acquistansi o con le armi di altri o con le proprie, o per fortuna o per virtù."); //inserire nome e cognome con il primo verso della vostra canzone preferita.
     }
 }
