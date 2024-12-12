0b7a951cfb321de41482e335f2bfae6515278720
Merry <rozzimario18@gmail.com>
2024-12-12 11:46:42 +0100

    mario commit for branch mario-branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(""); //inserire nome e cognome con il primo verso della vostra canzone preferita.
+        System.out.println("Mario Rozzi / Two-faced, caught in the middle"); //inserire nome e cognome con il primo verso della vostra canzone preferita.
     }
 }
